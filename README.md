3ac2f8f346cb586bcff4fe5affb1bb3b5e2dd2de
Cesar <cecerhcp0@hotmail.com>
2017-09-15 14:03:28 -0300

    added first version of the bot

---
+++ README.md
@@ -0,0 +1,2 @@
+## Rap quote bot
+Another simple project, trying to understand better ways to deal with callback hell in javascript and using Genius api to make a Twitter bot
---
+++ bot.js
@@ -0,0 +1,93 @@
+var twit = require('twit');
+var config = require('./config.js');
+var request = require('request');
+var BASE_URL = 'https://api.genius.com/';
+var Twitter = new twit(config);
+var geniusAccessToken = process.env.GENIUS_ACCESS_TOKEN;
+var songName = "bla"
+var songUrl = "bla"
+var artists = ["1421", "56", "22", "105", "59", "12417", "21", "48147"] //kendrick, nas, biggie, outkast, 2pac, rakim, wu tang, rtj
+
+var halfHour = 30*60*1000; // minutes*seconds*milliseconds
+
+
+var getRandomSongFromArtist = function(artistId) {
+	request({
+	  url: "https://api.genius.com/artists/" + artistId + "/songs?per_page=50",
+	  auth: {
+	    'bearer': geniusAccessToken
+	  }
+	}, function(err, res) {
+	  var obj = JSON.parse(res.body)
+	  var songs = obj["response"]["songs"]
+	  var i = Math.floor(Math.random() * songs.length)
+	  var songId = songs[i]["id"]
+	  songName = songs[i]["full_title"] 
+	  console.log(songName)
+	  songUrl = songs[i]["url"]
+	  getRandomQuoteFromSong(songId)
+	});
+}
+
+var getRandomQuoteFromSong = function(songId) {
+	request({
+	  url: "https://api.genius.com/referents?song_id=" + songId,
+	  auth: {
+	    'bearer': geniusAccessToken
+	  }
+	}, function(err, res) {
+	  var obj = JSON.parse(res.body)
+	  var quotes = obj["response"]["referents"]
+	  if (quotes.length == 0) { 
+	  	routine();
+	    return;
+	  }
+	  var i = Math.floor(Math.random() * quotes.length)
+	  var quote = quotes[i]["fragment"];
+	  if (quote.length <= 138) {
+	  	quote = "\"" + quote + "\""
+	  	// console.log(quote)
+	  	// tweetQuote(quote)
+ } else {
+ 	 routine()
+ }
+	});
+}
+
+var tweetQuote = function(quote) {
+	Twitter.post('statuses/update', { status: quote}, function(err, data, response) {
+  	console.log("tweetou " + quote)
+  	Twitter.get('statuses/user_timeline', { screen_name: 'rapquotebot', count: 1}, function(err, data, response) {
+		  var lastTweetId = data[0]["id_str"]
+		  tweetQuoteInfo(lastTweetId)
+		}) 
+  	})
+}
+
+var tweetQuoteInfo = function(statusId) {
+	quoteInfo = "@rapquotebot " + songName + "\n" + songUrl
+	if (quoteInfo.length > 140) {
+		quoteInfo = "@rapquotebot " + songUrl
+	}
+	Twitter.post('statuses/update', { status: quoteInfo, in_reply_to_status_id: statusId}, function(err, data, response) {
+  		console.log("tweetou " + quoteInfo) 
+  	})
+}
+
+
+var tweetRandomQuoteFromArtists = function(artists) {
+	var artistId = artists[Math.floor(Math.random() * artists.length)]
+	getRandomSongFromArtist(artistId)
+}
+
+var routine = function() {
+	tweetRandomQuoteFromArtists(artists)
+}
+
+
+tweetRandomQuoteFromArtists(artists)
+setInterval(routine, 1000 * 2); // 30 minutes interval
+
+
+
+
